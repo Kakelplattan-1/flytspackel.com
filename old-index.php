885f14672e8e8ce2a7ec8a4a9f12af630ae13d9e
Felix Wetell <felix.wetell@gmail.com>
2019-10-01 19:03:30 +0200

    Replaced old index view

---
+++ old-index.php
@@ -0,0 +1,205 @@
+		<?php
+			include "include/topp.php";
+		?>	
+<!-- Image Slider -->
+	<div class="container">
+		<div class="row welcome text-center">
+			<div id="slides" class="carousel slide" data-ride="carousel">
+				<ul class="carousel-indicators">
+					<li data-target="#slides" data-slide-to="0" class="active"></li>
+					<li data-target="#slides" data-slide-to="1"></li>
+					<li data-target="#slides" data-slide-to="2"></li>					
+				</ul>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<a href="infor-flytspackling.php">					
+							<img class="img-fluid rounded mx-auto d-block w-100%" src="img/flytspackel-elektrisk-golvvärme-4.png"; />
+							<div class="carousel-caption">
+								<h1>
+									Flytspacklar
+								</h1>
+								<h3>
+									Hur och vad, för bra resultat!
+								</h3>
+								<p>
+									Inför flytspackling
+						</a> 
+									och
+									<a href="dax-for-flytspackling.php">
+										Dax att flytspackla
+									</a>
+								</p>
+						</div>
+					</div>	
+					<div class="carousel-item">
+						<a href="flytspackel-kontakt.php">
+							<img class="img-fluid rounded mx-auto d-block w-100%" src="img/flytspackel-elektrisk-golvvärme-2.png"; />
+							<div class="carousel-caption">
+								<h1>
+									Frågor!
+								</h1>
+								<h3>
+									E-posta frågorna så hjälper vi dig!
+								</h3>	
+								<p>
+										Kontaktformuläret.
+								</p>	
+							</div>
+						</a>						
+					</div>	
+					<div class="carousel-item">
+						<a href="offert-flytspackel.php">					
+							<img class="img-fluid rounded mx-auto d-block w-100%" src="img/flytspackel-elektrisk-golvvärme-3.png"; />
+							<div class="carousel-caption">	
+								<h1>
+									Offertmallen!
+								</h1>
+								<h3>
+									Pris, mängder samt arbetsbeskrivning!
+								</h3>
+								<p>
+									Fyll i offertmallen
+								</p>
+							</div>	
+						</a>						
+					</div>					
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- Reklam -->	
+	<div class="container">
+		<div class="row welcome text-center">	
+			<div class="card-deck"> <!-- samma höjd på korten -->
+				<div class="card border-success mb-3" style="max-width: 30rem;">
+					<a href="offert-flytspackel.php" class="btn stretched-link">
+						<div class="card-header bg-transparent border-success">
+							<h4 class="offert-h4-a";>
+								Kiilto Floor Heat DF 
+							</h4>	
+							<h5 class="offert-h5-a";>
+								3-50 mm bygghöjd.
+							</h5>
+						</div>
+						<div class="card-body text-success">
+							<h5 class="card-title">
+								Flytspackel för golvvärme | hall | badrum
+							</h5>
+							<p class="card-text">
+								Flytspackel för avjämning | fallbyggnad | tillskjutande fukt | pumpbar.
+							</p>							
+								<img src="img/Kiilto-Floor-Heat.jpg" alt="Kiilto Floor Heat" title="Kiilto Floor Heat" class="img-fluid" />			
+						</div>
+						<div class="card-footer bg-transparent border-danger">
+							<h5 class="text-danger">						
+								135 kr / 20 kg säcken
+							</h5>
+						</div>
+						<p class="offert-p-a";>
+							Klicka för offert med frakt och tillbehör.
+						</p>
+					</a>	
+				</div>
+				<div class="card border-success mb-3" style="max-width: 30rem;">
+					<a href="offert-flytspackel.php" class="btn stretched-link">
+						<div class="card-header bg-transparent border-success">
+							<h4 class="offert-h4-a";>						
+								Kiilto Plan Rapid
+							</h4>	
+							<h5 class="offert-h5-a";>
+								5-50 mm bygghöjd.
+							</h5>							
+						</div>
+						<div class="card-body text-success">
+							<h5 class="card-title">
+								Flytspackel för golvvärme | plana ytor | tillskjutande fukt | pumpbar.
+							</h5>
+							<p class="card-text">
+								Flytspackel för hall, entre och plana ytor. Bra till golvvärme.
+							</p>						
+							<img src="img/Kiilto-Plan-Rapid.jpg" alt="Kiilto Plan Rapid" title="Kiilto Plan Rapid" class="img-fluid" />						
+						</div>
+						<div class="card-footer bg-transparent border-danger">
+							<h5 class="text-danger">						
+								165 kr / 25 kg säcken
+							</h5>							
+						</div>
+						<p class="offert-p-a";>
+							Klicka för offert med frakt och tillbehör.
+						</p>
+					</a>	
+				</div>
+				<div class="card border-success mb-3" style="max-width: 30rem;">
+					<a href="offert-flytspackel.php" class="btn stretched-link">				
+						<div class="card-header bg-transparent border-success">
+							<h4 class="offert-h4-a";>					
+								Elektrisk golvvärme
+							</h4>
+							<h5 class="offert-h5-a";>
+								För betong- och spånskivegolv.
+							</h5>
+						</div>
+						<div class="card-body text-success">
+							<h5 class="card-title">
+								Handy Heat elektriska golvvärmekit
+							</h5>
+							<p class="card-text">
+								20 års garanti, kvalité behöver inte kosta mycket. Digital, robust termostat. Dansktillverkning.
+							<img src="img/Elektrisk-golvvärme.jpg" alt="Elektrisk golvvärme" title="Elektrisk golvvärme" class="img-fluid" />	
+							</p>
+						</div>
+						<div class="card-footer bg-transparent border-danger">
+							<h5 class="text-danger">	
+								Från 895 kr ink digital termostat
+							</h5>
+						</div>
+						<p class="offert-p-a";>
+							Klicka för offert med frakt och tillbehör.
+						</p>						
+					</a>
+				</div>
+				<div class="card border-success mb-3" style="max-width: 30rem;">
+					<a href="offert-flytspackel.php" class="btn stretched-link">				
+						<div class="card-header bg-transparent border-success">
+							<h4 class="offert-h4-a";>					
+								Maskinblandare proffs
+							</h4>
+							<h5 class="offert-h5-a";>
+								För flytspackel, fix och fog.
+							</h5>
+						</div>
+						<div class="card-body text-success">
+							<h5 class="card-title">
+								Robust med positiv visp | blandare | 1400 watt
+							</h5>
+						<p class="card-text">
+							Effekt: 1400 W. Mjuk start. M14 fäste. 2 växlar. Vispen ingår.
+						<img src="img/maskinblandare.jpg" alt="Maskinblandare med visp" title="Maskinblandare med visp" class="img-fluid" />
+						</p>
+						</div>
+						<div class="card-footer bg-transparent border-danger">
+							<h5 class="text-danger">
+								20% rabatt = 1199 kr
+							</h5>
+						</div>
+						<p class="offert-p-a";>
+							Klicka för offert med frakt och tillbehör.
+						</p>						
+					</a>
+				</div> 
+			 </div>
+		</div>
+	</div>
+<!-- Fot -->
+		<?php
+			include "include/fot.php";
+		?>		
+
+
+<!--
+Filmer:
+	Grundläggande->https://www.youtube.com/watch?v=hnCmSXCZEpU
+	Detalj ->https://www.youtube.com/watch?v=9cKsq14Kfsw
+	Sass ->https://www.youtube.com/watch?v=roywYSEPSvc
+	Detalj och lång->https://www.youtube.com/watch?v=Uhy3gtZoeOM
+ -->
