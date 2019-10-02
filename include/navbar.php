2a3ff5af28bbfc31e0854955851dd17f229a1383
Felix Wetell <felix.wetell@gmail.com>
2019-10-02 12:27:34 +0200

    Created navbar partial #4 #2

---
+++ include/navbar.php
@@ -0,0 +1,34 @@
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <a href="/" class="navbar-brand">
+            <img src="img/flytspackel-logo.gif" width="180" alt="Support att göra flytspacklingen själv" title="Support att göra flytspacklingen själv" class="img-fluid rounded" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Hem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="offert-flytspackel.php">Offert på flytspackel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="faq-flytspackel.php">Frågor om flytspackel FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.youtube.com/user/KakelplattanAB/videos" target="_blank">Youtube-kanalen ^</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="flytspackel-om-oss.php">Om oss</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="flytspackel-kontakt.php">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main>
