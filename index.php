34a579fbd61a382367df00a8d43d84115867ef4d
Felix Wetell <felix.wetell@gmail.com>
2019-10-02 12:28:05 +0200

    Added carousel on start

--- index.php
+++ index.php
@@ -1,7 +1,71 @@
 <?php include 'include/head.php'; ?>
 <?php include 'include/navbar.php'; ?>
-<?php include 'include/support.php'; ?>
 
-    
+    <section class="containerladning-page-section">
+        <div class="carousel slide" data-ride="carousel" id="carouselIndicators">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/flytspackel-elektrisk-golvvärme-4.png" alt="">
+                    <div class="carousel-caption">
+                        <h2>
+                            Flytspacklar
+                        </h2>
+                        <h3>
+                            Hur och vad, för bra resultat!
+                        </h3>
+                        <p>
+                            Inför flytspackling
+                        </a>
+                            och
+                            <a href="dax-for-flytspackling.php">
+                                Dax att flytspackla
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/flytspackel-elektrisk-golvvärme-2.png" alt="">
+                    <div class="carousel-caption">
+                        <h2>
+                            Frågor!
+                        </h2>
+                        <h3>
+                            E-posta frågorna så hjälper vi dig!
+                        </h3>
+                        <p>
+                                Kontaktformuläret.
+                        </p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/flytspackel-elektrisk-golvvärme-3.png" alt="">
+                    <div class="carousel-caption">
+                        <h2>
+                            Offertmallen!
+                        </h2>
+                        <h3>
+                            Pris, mängder samt arbetsbeskrivning!
+                        </h3>
+                        <p>
+                            Fyll i offertmallen
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
 
 <?php include 'include/footer.php'; ?>
