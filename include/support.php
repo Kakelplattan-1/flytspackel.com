351a9305157dea90c9f2e59316b5aa8cab9f85d8
Felix Wetell <felix.wetell@gmail.com>
2019-10-02 12:25:47 +0200

    Created support partial #4

---
+++ include/support.php
@@ -0,0 +1,26 @@
+<section class="section container my-5 py-5" id="support">
+    <div class="d-block row d-md-flex flex-md-row">
+        <div class="col-md-7 d-flex align-items-center justify-content-center">
+            <div>
+                <h3>
+                    Känner du dig osäker?
+                </h3>
+                <p>
+                    Tveka inte att höra av dig. Vi ger dig support fram till 21:00 och har
+                    har alla bra råd. (exempeltext)
+                    <br>
+                    Ring oss och låt oss hjälpa dig med. Vill du skicka ett mail istället, så kan du använda
+                    länken nedan för att komma till kontaktformuläret. (exempeltext)
+                    <br>
+                    <b>Telefon:</b> <a class="link color" href="tel:054188180">054-18 81 80</a>
+                </p>
+                <a href="flytspackel-kontakt.php" class="btn btn-primary btn-expand">Kontakta oss</a>
+            </div>
+        </div>
+        <br class="d-block d-md-none">
+        <div class="col-md-5 d-flex align-items-center justify-content-center">
+            <img src="img/support-badrum.gif" alt="Support att göra badrum själv" title="Support att göra badrum själv" class="img-fluid" />
+        </div>
+    </div>
+
+</section>
