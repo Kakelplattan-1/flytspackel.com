3be0124b4ccbffd1c62fde36016f876d7b2d38ae
Felix Wetell <felix.wetell@gmail.com>
2019-11-10 12:37:15 +0100

    Adds information to footer #3

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,56 @@
         </main>
-        <footer>
-
+        <footer class="footer pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="img/flytspackel-logo.gif" alt="Support att göra flytspacklingen själv" title="Support att göra flytspacklingen själv" class="img-fluid rounded" />
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Kakelplattan AB</h4>
+                        <hr>
+                        <p>
+                            Hämtlager, ring/e-posta innan:
+                            <br>
+                            Industrigatan 1
+                            <br>
+                            665 33 Kil
+                            <br>
+                            Org-nr: 556682-6490
+                            <br>
+                            F-skatt finns
+                        </p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Företaget</h4>
+                        <hr>
+                        <p>
+                            <a href="flytspackel-kontakt.php">Kontaktformulär</a>
+                            <br>
+                            <a href="mailto:info@kakelplattan.se">info (at) kakelplattan.se</a>
+                            <br>
+                            <a href="www.kakelplattan.se">www.kakelplattan.se</a>
+                            <br>
+                            <a href="www.flytspackel.com">www.flytspackel.com</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Support</h4>
+                        <hr>
+                        <p>
+                            Support till 21:00 året om:
+                            <br>
+                            054-18 81 80
+                            <br>
+                        </p>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <br>
+                    <p>
+                        Copyright © <?php echo date( "Y" ) ?>
+                    </p>
+                </div>
+            </div>
         </footer>
     </body>
 </html>
