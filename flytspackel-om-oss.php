7133cf4522421a59e944b0ddd50208ac1e2f4ef0
Felix Wetell <felix.wetell@gmail.com>
2019-11-10 13:34:57 +0100

    Redesigns about view

--- flytspackel-om-oss.php
+++ flytspackel-om-oss.php
@@ -1,23 +1,24 @@
-﻿<?php include 'include/topp.php';?>	
-<!-- Image Slider -->
-	<div class="container text-center">
-	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-		<h1>Kakelplattan AB</h1>
-		<br />
-		<h2>Hur Kakelplattan.se är annorlunda.</h2> 
-		<br />
-		<p>
-		Kakelplattan erbjuder ett brett sortiment av badrum till så låga priser att så många som möjligt ska ha råd att köpa dem. 
-		Det är kärnan i allt som Kakelplattan gör, från fabrik och inköp till hur vi säljer våra produkter i hela Sverige. 
-		Vem som helst kan göra en produkt med hög kvalitet till ett högt pris, eller en produkt med låg kvalitet till ett lågt pris. 
-		Men att erbjuda bra produkter till låga priser är inte lika enkelt. 
-		Då måste man tänka och hitta lösningar som både sparar pengar och är annorlunda. 
-		Det har varit Kakelplattan:s fokus sedan företaget startades i Karlstad. 
-		Genom att köpa in stora mängder och satsa på tidlösa produkter efter människors behov och önskningar kan vi hålla kostnaderna låga. 
-		Har Ni frågor så ring oss gärna 054- 18 81 80 eller e-posta info@kakelplattan.se 
-		</p>
-	</div></div>
-<?php include 'include/fot.php';?>
+<?php include 'include/head.php'; ?>
+<?php include 'include/navbar.php'; ?>
+
+
+<section class="container about">
+	<h1>Kakelplattan AB</h1>
+	<h2>Hur <a class="link color" href="www.kakelplattan.se">www.kakelplattan.se</a> är annorlunda</h2>
+	<br>
+	<p>
+		Kakelplattan erbjuder ett brett sortiment av badrum till så låga priser att så många som möjligt ska ha råd att köpa dem.
+		Det är kärnan i allt som Kakelplattan gör, från fabrik och inköp till hur vi säljer våra produkter i hela Sverige.
+		Vem som helst kan göra en produkt med hög kvalitet till ett högt pris, eller en produkt med låg kvalitet till ett lågt pris.
+		Men att erbjuda bra produkter till låga priser är inte lika enkelt.
+		Då måste man tänka och hitta lösningar som både sparar pengar och är annorlunda.
+		Det har varit Kakelplattan:s fokus sedan företaget startades i Karlstad.
+		Genom att köpa in stora mängder och satsa på tidlösa produkter efter människors behov och önskningar kan vi hålla kostnaderna låga.
+		Har Ni frågor så ring oss gärna <a class="link color" href="tel:054188180">054-18 81 80</a> eller e-posta <a class="link color" href="info@kakelplattan.se">info (at) kakelplattan.se</a>
+	</p>
+</section>
+
+<?php include 'include/footer.php';?>
 
 
 
