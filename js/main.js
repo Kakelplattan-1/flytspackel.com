2c46c723c648f2c60def8122aa3743993396b9b1
Felix Wetell <felix.wetell@gmail.com>
2019-12-21 11:18:07 +0100

    Fixes #9, adds cookie consent

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-'user strict';
+'use strict';
+
+$( document ).ready(function() {
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#000"
+        },
+        "button": {
+          "background": "#245733"
+        }
+      },
+      "theme": "classic",
+      "position": "bottom-right",
+      "content": {
+        "message": "Den här webbplatsen använder kakor för att du ska få den bästa upplevelsen på vår hemsida.",
+        "dismiss": "Uppfattat",
+        "link": "Läs mer här"
+      }
+    });
+});
